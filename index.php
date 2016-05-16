3a1de70bd1bbe6c2d36408af6095049792c92dc0
Gustavo Guichard <gustavoguichard@gmail.com>
2016-05-16 16:54:52 -0300

    Fix more responsive issues

--- index.php
+++ index.php
@@ -12,15 +12,15 @@
 			<li><a href="<?php bloginfo('url');?>" rel="tudo" title="Filtrar portfólio">Tudo</a></li>
 		<?php endif;?>
 		<?php foreach($tags as $tag):?>
-			<?php 
+			<?php
 				$pattern = '/(\w+)((\_\$)+(\w+)$)/i';
 				$replacement = '${1}';
 				$tag_readable = preg_replace($pattern, $replacement, $tag->name);
-			;?>	
+			;?>
 			<li><a href="<?php bloginfo('url');?>/tag/<?=$tag->slug;?>" rel="tag-<?=$tag->slug;?>" title="Filtrar portfólio"><?=$tag_readable;?></a></li>
 		<?php endforeach;?>
 	</ul>
-	<article id="folio_container">
+	<article id="folio_container row">
   <?php query_posts(array($query_string, 'showposts'=>'-1', 'post_type'=>'post', 'orderby'=>'menu_order', 'order'=>'ASC'));?>
 	<?php get_template_part( 'loop', 'index' ); ?>
 	</article>
