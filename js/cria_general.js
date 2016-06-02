427be8ac12648af00a036eef029ea7932956c298
Gustavo Guichard <gustavoguichard@gmail.com>
2016-06-02 14:12:03 -0300

    Refinement

--- js/cria_general.js
+++ js/cria_general.js
@@ -3,13 +3,13 @@ $(function() {
   $(".fancybox").fancybox();
 
   $('.criador_thumb').on('click', function(e){
-    $('.criador_thumb').fadeOut();
-    $('.criador_bio', $(this).parent()).slideDown();
+    $('.criador_thumb').hide();
+    $('.criador_bio', $(this).parent()).fadeIn();
   	e.preventDefault();
   });
 
   $('.criador_bio a.criador_foto, .criador_bio a.close_criador, .criador_bio h3').on('click', function(e){
-    $(this).parents('.criador_bio').fadeOut();
+    $(this).parents('.criador_bio').hide();
     $('.criador_thumb').fadeIn();
   	e.preventDefault();
   });
